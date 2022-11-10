fdc7e80ec6914b70f9d59cdf3e048eaddd182b25
Hp <Hp@DESKTOP-ID5FAVQ>
2022-11-10 22:53:35 +0100

    tache

---
+++ TP3EXERCICE3/Cercle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3EXERCICE3
+{
+    public class Cercle
+    {
+        Point centre;
+        double rayon;
+
+
+        public Cercle() { }
+
+        public Cercle( Point c , double r) 
+        
+        {
+            centre.y = c.y;
+            centre.x = c.x;
+            rayon = r;
+        
+        
+        }
+
+        public Point getCentre()
+        {
+
+            return centre;
+        }
+
+        public double getRayon()
+        {
+
+            return rayon;
+        }
+
+
+        public void  setCentre( Point c1 )
+        {
+            centre.x = c1.x;
+            centre.y=c1.y;  
+
+        }
+
+        public void setRayon(double r)
+        {
+            rayon = r;
+           
+
+        }
+
+
+    }
+}
